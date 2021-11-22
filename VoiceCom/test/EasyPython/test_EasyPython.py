b1ef99dd0d0c3117fe4cca43a5c4bb2237b620d8
kwankunghkg <92127036+kwankunghkg@users.noreply.github.com>
2021-11-22 11:47:28 +0800

    Add files via upload

---
+++ VoiceCom/test/EasyPython/test_EasyPython.py
@@ -0,0 +1,18 @@
+################################
+##Generated with a lot of love##
+##    with   EasyPython       ##
+##Web site: easycoding.tn     ##
+################################
+
+variable_A = None
+variable_B = None
+
+
+variable_A = 'Hello'
+variable_B = 3
+for count in range(3):
+  print('abc')
+  print('123')
+  print(variable_A)
+  print(variable_B)
+  print(variable_B * 3)
