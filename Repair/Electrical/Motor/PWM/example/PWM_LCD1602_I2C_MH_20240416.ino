f0b7f685b84db78c9f2858de33e93185a75c7e32
kwankunghkg <92127036+kwankunghkg@users.noreply.github.com>
2024-04-16 14:08:22 +0800

    Create PWM_LCD1602_I2C_MH_20240416.ino

---
+++ Repair/Electrical/Motor/PWM/example/PWM_LCD1602_I2C_MH_20240416.ino
@@ -0,0 +1,46 @@
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// https://www.nxp.com/documents/data_sheet/PCF8574.pdf
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display NXP PCF8574T
+// LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display NXP PCF8574AT (MH)
+
+int analogInPin = A0;   // potentiometer connected to analog pin 3
+int pot = 0;            // variable to store the analog read value
+char display_string[16] = ""; //  Buffer for 40 character width
+  
+void setup()
+{
+  Serial.begin(115200);            // open the serial port 
+  lcd.init();                      // initialize the lcd
+
+  // Print a message to the LCD.
+  lcd.backlight();
+  // lcd.print("Hello, world!");
+
+  pinMode(LED_BUILTIN, OUTPUT);  // sets the pin as output
+}
+
+void loop()
+{
+  pot = analogRead(analogInPin);  // read the input pin
+  Serial.println(pot);
+  analogWrite(LED_BUILTIN, pot / 4); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
+
+  // set the cursor to column 0, line 0
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+  // print the number of seconds since reset:
+  lcd.print("TIME : ");
+  lcd.print(millis()/1000);
+  // set the cursor to column 0, line 1
+  lcd.setCursor(0, 1);
+  lcd.print("VR   : ");
+  //lcd.print(pot, DEC);
+  //sprintf(display_string,"%2d %2d %4.2f %7.3e",v1,v2,v3,v4);
+  sprintf(display_string,"%3d  ", pot);
+  lcd.print( display_string );
+}
